fce115bff495400e0804b13f621aa93879fb40a4
wujiangwei <wujiangwei123@126.com>
2019-08-27 18:27:09 +0800

    2019-08-27

--- 0813/QuestionAnswerController.class.php
+++ 0813/QuestionAnswerController.class.php
@@ -58,13 +58,10 @@ class QuestionAnswerController extends BaseController
             ->where($user_where)
             ->order("a.created_at desc")
             ->count();
-
-
         //被采纳的个数
         $integral_num = M('designer_answer as a')
             ->join("user_questions u on a.a_questions_id = u.q_id", 'left')
-            ->where($user_where)
-            ->where("a.a_status = 3")
+            ->where("a.a_mid = '".$this->userInfo['mid']."' AND a.a_status = 3")
             ->order("a.created_at desc")
             ->count();
 
@@ -78,29 +75,45 @@ class QuestionAnswerController extends BaseController
                 ->order("a.created_at desc")
                 ->limit(($nowPage-1)*$pageSize,$pageSize)
                 ->select();
+
             $count = M('designer_answer as a')
                 ->join("user_questions u on a.a_questions_id = u.q_id", 'left')
                 ->where($user_where)
                 ->order("a.created_at desc")
                 ->count();
 
-        } else {
+        } elseif ($status == 3) {
             //已被采纳的
             $answer_data = M('designer_answer as a')
                 ->join("user_questions u on a.a_questions_id = u.q_id", 'left')
-                ->where($user_where)
-                ->where("a.a_status = 3")
+                ->where("a.a_mid = '".$this->userInfo['mid']."' AND a.a_status = 3")
                 ->group("a.a_questions_id")
                 ->order("a.created_at desc")
                 ->limit(($nowPage-1)*$pageSize,$pageSize)
                 ->select();
+
             $count = M('designer_answer as a')
                 ->join("user_questions u on a.a_questions_id = u.q_id", 'left')
-                ->where($user_where)
-                ->where("a.a_status = 3")
+                ->where("a.a_mid = '".$this->userInfo['mid']."' AND a.a_status = 3")
                 ->order("a.created_at desc")
                 ->count();
 
+        } else {
+            $user_where .= ' AND a.status != 1';
+            //邀请设计师回答信息
+            $answer_data = M('invitation_answer as a')
+                ->join("user_questions u on a.a_questions_id = u.q_id", 'left')
+                ->where($user_where)
+                ->order("a.created_at desc")
+                ->limit(($nowPage-1)*$pageSize,$pageSize)
+                ->select();
+
+            $count = M('invitation_answer as a')
+                ->join("user_questions u on a.a_questions_id = u.q_id", 'left')
+                ->where($user_where)
+                ->order("a.created_at desc")
+                ->count();
+            $answer_num = $count;
         }
 
         //分页
@@ -136,6 +149,13 @@ class QuestionAnswerController extends BaseController
                 $questions_id = I('questions_id');
                 $content = I('content');
                 $ids = I('ids', '');
+
+                //判断当前回答是否有邀请
+                if(M('invitation_answer')->where("a_questions_id = $questions_id AND a_mid = '".$this->userInfo['mid']."'")->find()) {
+                    M('invitation_answer')->where("a_questions_id = $questions_id AND a_mid = '".$this->userInfo['mid']."'")->save(['status' => 1]);
+                }
+
+                //设计师修改解答
                 if($ids != ''){
                     $data = [
                         'a_answer' => $content,
@@ -381,15 +401,20 @@ class QuestionAnswerController extends BaseController
             $this->ajaxReturn(array('state' => 1102, 'message' => '非法请求！'));
         }
 
+        //判断是否登录
         if(!$this->userInfo['mid']) {
             $this->ajaxReturn(array('state' => 1102, 'message' => '亲，要先登录哦…'));
         }
+
         //接值
         $a_id = trim(I('id'));    //解答自增id
+
         //获取一条解答的数据
         $answer_data = M("designer_answer")->where("a_id = $a_id")->find();
+
         //获取问题的标题内容
         $questions_data = M('user_questions')->where("q_id = ".$answer_data['a_questions_id'])->find();
+
         //条件
         $where['answer_id'] = $a_id;
         $where['user_ip'] = getIP();
@@ -410,7 +435,7 @@ class QuestionAnswerController extends BaseController
 
             $data                 = [];
             $ip                   = get_client_ip();
-            $data['mid']          = $questions_data['q_mid'];
+            $data['mid']          = $this->userInfo['mid'];                 //当前登录者的mid
             $data['user_no']      = '';
             $data['user_type']    = 0;
             $data['creat_time']   = date("Y-m-d H:i:s", time());
@@ -418,12 +443,12 @@ class QuestionAnswerController extends BaseController
             $data['ip']           = $ip;
             $data['praiseid_no']  = $answer_data['a_questions_id'];
             $data['title']        = $questions_data['q_title'];
-            $data['receive_mid']  = $answer_data['a_mid'];
+            $data['receive_mid']  = $answer_data['a_mid'];                  //设计师mid
             $data['receive_type'] = 1;  //方案的被点赞人类型只能是设计师
             $res                  = M('thumbs_up_log')->data($data)->add();
 
             //返回提示
-            $this->ajaxReturn(array('state' => 1100, 'message' => '点击成功'));
+            $this->ajaxReturn(array('state' => 1100, 'message' => '满意+1'));
         } else {
             //已经点击过了
             $this->ajaxReturn(array('state' => 1102, 'message' => '点一次就好了，不可贪心哦'));
@@ -445,10 +470,17 @@ class QuestionAnswerController extends BaseController
             $this->ajaxReturn(array('state' => 1102, 'message' => '非法请求！'));
         }
 
+        //判断是否登录
+        if(!$this->userInfo['mid']) {
+            $this->ajaxReturn(array('state' => 1102, 'message' => '亲，要先登录哦…'));
+        }
+
         //接值
         $a_id = trim(I('id'));    //解答自增id
+
         //获取一条解答的数据
         $answer_data = M("designer_answer")->where("a_id = $a_id")->find();
+
         //条件
         $where['answer_id'] = $a_id;
         $where['user_ip'] = getIP();
@@ -467,7 +499,7 @@ class QuestionAnswerController extends BaseController
             M("designer_answer")->where("a_id = $a_id")->save(["no_satisfied" => $num]);
 
             //返回提示
-            $this->ajaxReturn(array('state' => 1100, 'message' => '点击成功'));
+            $this->ajaxReturn(array('state' => 1100, 'message' => '不满意+1'));
         } else {
             //已经点击过了
             $this->ajaxReturn(array('state' => 1102, 'message' => '点一次就好了，不可贪心哦'));
@@ -486,16 +518,17 @@ class QuestionAnswerController extends BaseController
     {
         // 设计师mid
         $mid = $this->userInfo['mid'];
+
         //当前页和每页显示的个数
         $nowPage = I('page',1,'int');
         $pageSize = 50;
+
         //总条数
         $count = M('designer_integral')->where("a_mid = '$mid'")->count();
 
         //获取积分
         $data = M('designer_integral d')->join("user_questions as u on d.a_questions_id = u.q_id", 'left')->field("d.*, u.q_title")->where("d.a_mid = '$mid'")->order("created_at desc")->limit(($nowPage-1)*$pageSize,$pageSize)->select();
         $sum = M('answer_adopt_num')->field('integral, consumption_integral')->where("a_mid = '$mid'")->find();
-
 
         //分页
         $Page = new \Org\Util\PageNewThd($count,I('get.'),$pageSize,$nowPage);// 实例化分页类 传入总记录数和每页显示的记录数
@@ -533,12 +566,14 @@ class QuestionAnswerController extends BaseController
             Db::startTrans();
             try {
 
+                // 获取积分总数
                 $jifen = M('answer_adopt_num')->where("a_mid = '".$this->userInfo['mid']."'")->getField('integral');
-                $consumption_integral = M('answer_adopt_num')->where("a_mid = '".$this->userInfo['mid']."'")->getField('integral');
+                $consumption_integral = M('answer_adopt_num')->where("a_mid = '".$this->userInfo['mid']."'")->getField('consumption_integral');
 
                 if($fen_data['fen'] > $jifen){
-                    $this->ajaxReturn(array('start' => 1102, 'message' => '积分不足'));
+                    $this->ajaxReturn(array('state' => 1102, 'message' => '积分还差那么一丢丢…'));
                 }
+                // 计算积分并修改入库
                 $integral = $jifen - $fen_data['fen'];
                 $consumption_integral += $fen_data['fen'];
                 M('answer_adopt_num')->where("a_mid = '".$this->userInfo['mid']."'")->save(['integral' => $integral, 'consumption_integral' => $consumption_integral]);
@@ -555,16 +590,44 @@ class QuestionAnswerController extends BaseController
 
                 M('designer_integral')->add($arr);
 
-//                //获取设计师编号
-//                $designer_no = M('user_designer')->where("mid = '".$this->userInfo['mid']."'")->getField('designer_no');
-//
-//
-//                $farmcard_data = M('win_farmcard_info')->where("designer_no = '".$designer_no."'")->find();
-//                if(!$farmcard_data){
-//                    $this->ajaxReturn(array('state' => 1102, 'message' => '您还没有渲染卡'));
-//                }
-//
-//                M('win_farmcard_info')->where("")->save();
+
+                //获取设计师编号
+                $designer_no = M('user_designer')->where("mid = '".$this->userInfo['mid']."'")->getField('designer_no');
+
+                //获取渲染卡信息
+                $card_info = $this->getFarmcard($fen_data['dian'], $type = 4);
+
+                //生成新的渲染小卡
+                $str="QWERTYUIOPASDFGHJKLZXCVBNM1234567890qwertyuiopasdfghjklzxcvbnm";
+                str_shuffle($str);
+                $create_time = date('Y-m-d H:i:s');
+                $card_no = substr(date('Ymd'),2).($card_info['discern']).(10000+$card_info['already_create']);
+                $password=substr(str_shuffle($str),26,12);
+
+                //组合数据 & 入库
+                $setData = array();
+                $setData['farmcard_id'] = $card_info['id'];                     //渲染卡id
+                $setData['card_no'] = $card_no;
+                $setData['password'] = Encrypt($password);
+                $setData['password_md5'] = md5($card_info['id'].$password);
+                $setData['card_num'] =  $card_info['card_num'] ;
+                $setData['surplus_num'] = $card_info['card_num'] ;
+                $setData['create_time'] = $create_time;
+                //渲染卡关联设计师信息
+                $setData['status'] = 1;
+                $setData['activate'] = $create_time;
+                $setData['designer_no'] = $designer_no;
+                $setData['validity'] = date("Y-m-d",strtotime($create_time ."  +".$card_info['validity_days']."   day"));
+                $setData['activate_type'] = 0;
+                $setData['activate_uid'] = 0;
+                $setData['activate_remark'] = '';
+                $setData['audit_time'] = $create_time;
+
+                M('win_farmcard_info')->add($setData);
+
+                //兑换成功后积分卡减一
+                $card_info['already_create'] = $card_info['already_create'] + 1;
+                M('win_farmcard')->where("card_num = ".$fen_data['dian']." AND card_type = 4")->save($card_info);
 
                 Db::commit();
                 $this->ajaxReturn(array('state' => 1100, 'message' => '兑换成功'));
@@ -575,11 +638,45 @@ class QuestionAnswerController extends BaseController
         }
         $sum = M('answer_adopt_num')->field('integral, consumption_integral')->where("a_mid = '".$this->userInfo['mid']."'")->find();
 
-
         $this->assign('sum', $sum);
         $this->assign('arr', $arr);
 
         $this->view('QuestionAnswer/integralExchange', 3);
+    }
+
+    public function invitation () {
+        // 判断是否是ajax请求
+        if (!IS_AJAX) {
+            $this->ajaxReturn(array('state' => 1102, 'message' => '非法请求！'));
+        }
+
+        //接收设计师mid & 问题id
+        $mid = I('mid');
+        $a_questions_id = I('a_questions_id');
+
+        //组合条件
+        $where = array(
+            'a_mid' => $mid,
+            'a_questions_id' => $a_questions_id
+        );
+
+        //检查是否存在
+        if(!M('invitation_answer')->where($where)->count()) {
+            $where['status'] = 0;
+            $where['created_at'] = date('Y-m-d H:i:s', time());
+            $where['updated_at'] = date('Y-m-d H:i:s', time());
+            M('invitation_answer')->add($where);
+            $this->ajaxReturn(array('state' => 1100, 'message' => '邀请成功'));
+        }
+
+        $this->ajaxReturn(array('state' => 1102, 'message' => '邀请失败'));
+    }
+
+    public function getFarmcard($num, $type)
+    {
+        $id = M('win_farmcard')->where("card_num = $num AND card_type = $type")->find();
+
+        return $id;
     }
 
 
@@ -658,6 +755,9 @@ class QuestionAnswerController extends BaseController
      */
     public function addQuestions()
     {
+        //设置路由:普通模式
+        C('URL_MODEL','0');
+
         $id = trim(I('id', '', int));
         if (IS_POST) {
             $pic = array();
@@ -697,14 +797,13 @@ class QuestionAnswerController extends BaseController
                 'updated_at' => date('Y-m-d H:i:s', time())
             );
 
-
             if($id == ''){
                 //添加
                 $res = $this->questions->creates($data);
                 if($res) {
-                    $this->ajaxReturn(array('state' => 1100, 'message' => '添加成功'));
+                    $this->ajaxReturn(array('state' => 1100, 'message' => '发布成功'));
                 } else {
-                    $this->ajaxReturn(array('state' => 1102, 'message' => '添加失败'));
+                    $this->ajaxReturn(array('state' => 1102, 'message' => '发布失败'));
                 }
             } else {
                 //修改
