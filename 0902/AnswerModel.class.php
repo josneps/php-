d09b8403527c6b2eaa7e29152c91833785bc008a
wujiangwei <wujiangwei123@126.com>
2019-09-02 18:34:45 +0800

    2019-09-02

---
+++ 0902/AnswerModel.class.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Home\Model;
+
+use Think\Model;
+use think\Db;
+
+/**
+ * Class ClassName
+ * @package Home\Model
+ */
+class AnswerModel extends Model
+{
+    protected $tableName = 'designer_answer';
+
+
+}
